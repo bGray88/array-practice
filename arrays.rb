c9fae66f8aca37315173806d83fd0920986b4830
Brandon Gray <brandonjoe88@hotmail.com>
2022-08-25 12:02:00 -0600

    Added practice and explanations

--- arrays.rb
+++ arrays.rb
@@ -3,12 +3,13 @@ integers = [1, 5, 4, 3]
 floats = [1.9, 4.1, 5.6, 6.7]
 booleans = [true, false, true, true]
 
+# Part 1
 # This method will return the last element of the strings array ("ann") and remove it from the array
 p strings.pop
 p strings
 
 # This method will add the the integer 9 as the last element of the integers array
-p integers.push 9
+p integers.push(9)
 
 # This method will return the first element of the floats array (1.9) and remove it from the array
 p floats.shift
@@ -16,3 +17,13 @@ p floats
 
 # This method will add the boolean value of false as the first element of the booleans array
 p booleans.unshift false
+
+# Part 2
+# This call to the array element at index 1 will return the value 5.6 instead of the original value at index 1 that was 
+# initiallized when the array floats was declared (4.1) as that was removed using the shift method earlier
+p floats[1]
+
+# I will now add another element to the end of the floats array (7.9) using the method push and access it using the index 
+# by finding the current length of the array with the calculation of (length - 1) as indexes for arrays start at 0
+floats.push(7.9)
+p floats[floats.length - 1]
